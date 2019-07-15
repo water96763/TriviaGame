074571eec7a682c0a6c0cb05d87687edeb4ef183
Valerie Sakamura <water96763@gmail.com>
2019-07-15 00:11:36 -0700

    started over, new html and css and working on js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
-let start = document.getElementById("btn");
-let game = document.getElementById("activeGamePage");
-let question = document.getElementById("question");
+let start = document.getElementById("button");
+let game = document.getElementById("page");
+let question = document.getElementById("question-row");
 let imageForQuestion = document.getElementById("questionImg");
 let answerA = document.getElementById("A");
 let answerB = document.getElementById("B");
 let answerC = document.getElementById("C");
-let timer = document.getElementById("timer");
+let timer = document.getElementById("timer-row");
 let answer = document.getElementById("answer");
-let timer;
-
+let gameOver = document.getElementById("game-over")
+var timeleft = 120;
 // function hideScreen() {
 //     var x = document.getElementById("button");
 //     if (x.style.display === "none") {
@@ -16,83 +16,80 @@ let timer;
 //     } else {
 //       x.style.display = "none";
 // } 
-$(document).on("click", "#button", function() {
-  //  startGame(); 
-  askTheQuestion();
-});
-
-
-function startGame() {
-    timer = setInterval(counter, 100);
-    askTheQuestion();
-  //  hideScreen ();
-}
-
-        
-function askTheQuestion(){
-
-            
-        let questions = [
-            {   question : "According to Va, how much toilt paper does one person ue a month?",
-                imgSrc : "img/html.png",
-                answerA : "Correct",
-                answerB : "Wrong",
-                answerC : "Wrong",
+          let questions = [
+            {question : "What kind of pets does Val have?",
+                answerA : "Turtles and Tortoises",
+                answerB : "Finches and Parrots",
+                answerC : "Cats and Dogs"
                 correct : "A"
             },
-            {   question : "According to Va, how much toilt paper does one person ue a month?",
-                imgSrc : "img/html.png",
-                answerA : "Correct",
-                answerB : "Wrong",
-                answerC : "Wrong",
-                correct : "A"
-            },
-            {   question : "According to Va, how much toilt paper does one person ue a month?",
-                imgSrc : "img/html.png",
-                answerA : "Correct",
-                answerB : "Wrong",
-                answerC : "Wrong",
-                correct : "A"
-            },
-            {   question : "According to Va, how much toilt paper does one person ue a month?",
-                imgSrc : "img/html.png",
-                answerA : "Correct",
-                answerB : "Wrong",
-                answerC : "Wrong",
-                correct : "A"
-            },
-            {   question : "According to Va, how much toilt paper does one person ue a month?",
-                imgSrc : "img/html.png",
-                answerA : "Correct",
-                answerB : "Wrong",
-                answerC : "Wrong",
-                correct : "A"
-            },
-        ]      
+            {question : "Which Hawaiian island used to supply 75% of the pineapples in the world?",
                 
+                answerA : "Oahu",
+                answerB : "Waikiki",
+                answerC : "Lanai",
+                correct : "C"
+            },
+            {question : "Does Hawaii observe daylight savings?",
+               
+                answerA : "Yes",
+                answerB : "No",
+                answerC : "Sometimes",
+                correct : "B"
+            },
+            {question : "When someone from Hawaii says the word ma-ke, what does it mean?",
+              
+                answerA : "Tired",
+                answerB : "Dead",
+                answerC : "Single",
+                correct : "B"
+            },
+            {question : "What types of fruit trees are commonly found throughout Hawaii?",
+            
+                answerA : "Apple and Tangerine",
+                answerB : "Mango and Papaya",
+                answerC : "Coconut and Prune",
+                correct : "B"
+            },
+        ];     
+      
+function askTheQuestion()  {
         let lastQuestionIndex = questions.length - 1;
         let currentQuestionIndex = 0;
             for (i = 0; i < questions.length -1; i++) {
                 let q = questions[currentQuestionIndex];
-                question.innerHTML = "<p>" + q.question +"</p>";
-                imageForQuestion.innerHTML = "<img src=" + q.imageForQuestion +">";
-                answerA.innerHTML = q.answerA;
-                answerB.innerHTML = q.answerB;
-                answerC.innerHTML = q.answerC;
-            };
+//                 question.innerHTML = "<p>" + q.question +"</p>";
+//                 answerA.innerHTML = q.answerA;
+//                 answerB.innerHTML = q.answerB;
+//                 answerC.innerHTML = q.answerC;
+//             }
+//             if (currentQuestionIndex < lastQuestionIndex) {
+//                 currentQuestionIndex++;
+//                 askTheQuestion();
+//             }
+//             };
 
-        function counter() {
-            if (currentQuestionIndex < lastQuestionIndex) {
-                currentQuestionIndex++;
-                askTheQuestion();
-            };
-        };
-
-        function checkAnswer(answer) {
-            if (questions[currentQuestionIndex].correct == answer){
-                score++;
-            };
-            if(currentQuestionIndex < lastQuestionIndex) {
-                score.innerHTML = "<p>" + score + "<p>";
-            };
-        };
+//         function checkAnswer(answer) {
+//             if (questions[currentQuestionIndex].correct == answer){
+//                 score++;
+//             };
+//             if(currentQuestionIndex < lastQuestionIndex) {
+//                 score.innerHTML = "<p>" + score + "<p>";
+//             };
+//         };
+$(document).on("click", "#button", function() {
+    askTheQuestion();
+    hideScreen();
+  //   document.getElementById("startPage").style.visibility = "hidden";
+  //   document.getElementById("activeGamePage").style.visibility ="visible";
+    var downloadTimer = setInterval(function(){
+      document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+      timeleft -= 1;
+      if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Finished !";
+      }
+    }, 1000);
+    });
+//         askTheQuestion();
+//         checkAnswer();
