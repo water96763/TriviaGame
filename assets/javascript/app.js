79616d390f229c2e57804aaac1a3a24ce02b765f
Valerie Sakamura <water96763@gmail.com>
2019-07-18 14:20:52 -0700

    totally redone, radio and no counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,144 +1,120 @@
 
-$(document).ready(function() {
-let scoreCorrect = 0;
-let scoreIncorrect = 0;
-let unansweredQuestions = 0;
-let timeLeft = 20;
-let intervalID;
-let questionIndex = 0;
-let answered = false;
-let correctAnswer;
+// $(document).ready(function() {
 
-let questions = [
-                  {
-                  question: "What kind of pets does Val have?",
-                  
-                  answer: ["Turtles and Tortoises", 
-                           "Finches and Parrots",
-                          "Cats and Dogs"],
-                  correct: "A"
-                  },
-                  {question : "Which Hawaiian island used to supply 75% of the pineapples in the world?",
-                  answer: ["Oahu",
-                          "Waikiki",
-                          "Lanai"],
-                  correct : "C"
-                  },
-                  {question : "Does Hawaii observe daylight savings?",
-                  answer : ["Yes",
-                            "No",
-                            "Sometimes"],
-                  correct : "B"
-                  },
-              {question : "When someone from Hawaii says the word ma-ke, what does it mean?",
-                  answer: ["Tired",
-                          "Dead",
-                           "Single"],
-                  correct : "B"
-              },
-              {question : "What types of fruit trees are commonly found throughout Hawaii?",
-                  answer: ["Apple and Tangerine",
-                           "Mango and Papaya",
-                           "Coconut and Prune"],
-                  correct : "B"
-              },
-          ];     
-    function startGame() {
-        $('.start-button').remove();
-        correct = 0;
-        incorrect = 0;
-        unanswered = 0;
-        loadQuestions();
-        
-     function loadQuestions() {     
-          answered = false;
-          timeRemaining = 20;
-          intervalID = setInterval(timer, 1000);
-          if (answered === false) {
-              timer();
+
+let scoreRender = document.getElementById('scoreContainer')
+let start = document.getElementById('button');
+let summitButton = document.getElementById('submit');
+
+function generateQuiz() {
+  let questions = [
+    {
+    question: "What kind of pets does Val have?",
+    
+    answers: {
+            A: "Turtles and Tortoises", 
+            B: "Finches and Parrots",
+            C: "Cats and Dogs"
+            },
+    correct: "A"
+    },
+    {question : "Which Hawaiian island used to supply 75% of the pineapples in the world?",
+    answers: {
+            A: "Oahu",
+            B: "Waikiki",
+            C: "Lanai",
+            },
+    correct : "C"
+    },
+    {question : "Does Hawaii observe daylight savings?",
+    answers: {
+             A: "Yes",
+             B: "No",
+             C: "Sometimes",
+             },
+    correct : "B"
+    },
+{question : "When someone from Hawaii says the word ma-ke, what does it mean?",
+    answers: {
+            A: "Tired",
+            B: "Dead",
+            C: "Single",
+            },
+    correct : "B"
+},
+{question : "What types of fruit trees are commonly found throughout Hawaii?",
+    answers: {
+            A: "Apple and Tangerine",
+            B: "Mango and Papaya",
+            C: "Coconut and Prune",
+            },
+    correct : "B"
+},
+]; 
+
+  function showQuestions(){
+        let output =[];
+        let answers;
+        for(let i=0; i<questions.length; i++) {
+          answers = [];
+          for(letter in questions[i].answers) {
+            answers.push(
+              '<label>' + '<input type="radio" name="question' + i + '"value="' + letter + '">' + letter + ' : ' + questions[i].answers[letter] + '</label>'
+            );
           }
-          correctAnswer = questions[questionIndex].correct;
-          let question = questions[questionIndex].question;
-          $('.question').html(question);
-              for (let i = 0; i < 2; i++) {
-                let answer = questions[questionIndex].answer[i];
-                $('.answers').append('<h4 class= allAnswers id=' + i + '>' + answer + '</h4>');
-              }
-          $("h4").click(function() {
-            let id = $(this).attr('id');
-                if (id === correct)  {
-                  answered = true;
-                  $('.question').text("Correct!");
-                  correctAnswer();
-                } else {
-                  answered = true;
-                  $('question').text("Nope, the answer is " + questions[questionIndex].answer[correct]);
-                  incorrectAnswer();
-                }
-            }); 
- }
-    function timer() {
-      if (timeLeft === 0) {
-        answered = true;
-        clearInterval(intervalID);
-        $('question').text("The answer was " + questions[questionIndex].answer[correct]);
-        unanswered();
-      } else if (answered === true) {
-        clearInterval(intervalID);
-      } else {
-        timeLeft--;
-        $('.timeLeft').text('Time left= ' + timeLeft);
-      }
+          output.push(
+            '<div class="question">' + questions[i].question + '</div>' +
+            '<div class="answers">'
+              + answers.join(' ') + '</div>'
+          );
+          }
+          quizContainer.innerHTML = output.join(' ');
+  }    
+ 
+  function showResults() {
+    let answerContainers = quizContainer.querySelectorAll('.answers');
+    let userAnswer = '';
+    let numCorrect = 0;
+    for(let i=0; i<questions.length; i++){
+              userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      
+            if(userAnswer===questions[i].correct){
+              numCorrect++;
+              answerContainers[i].style.color = 'lightgreen';
+            }
+            else{
+              answerContainers[i].style.color = 'red';
+            }
     }
+          resultsContainer.innerHTML = "Correct: "  + numCorrect + "  Incorrect:" + (5-numCorrect);
+          
+  }
 
-    function correctAnswer() {
-      scoreCorrect++;
-      resetRound();
-    }
-
-    function incorrectAnswer() {
-      incorrectAnswers++;
-      resetRound();
-    }
-    function unansweredQuestions() {
-      unansweredQuestions++;
-      resetRound();
-    }
-
-    function resetRound() {
-      $('.allAnswers').remove();
-      questionIndex++;
-      if (questionIndex <questions.length) {
-        setTimeout(function () {
-          loadQuestions();
-        })
-      } 
-    }
+  showQuestions();
 
 
 
-// let start = document.getElementById("button");
-// let quiz = document.getElementById("quiz");
-// let question = document.getElementById("question");
-// let timer = document.getElementById("timer");
-// let choiceA = document.getElementById("A");
-// let choiceB = document.getElementById("B");
-// let choiceC = document.getElementById("C");
-// let scoreDiv = document.getElementById("scoreContainer");
-// let correct = document.getElementById("correct");
-// let losses = document.getElementById("losses");
-// 
-// let lastQuestion = questions.length - 1;
-// let runningQuestion = 0;
-// let score = 0;
-                    
-//                     function renderQuestion() {
-//                         let q = questions[runningQuestion];
-//                         question.innerHTML = "<p>"+ q.question +"</p>";
-//                         choiceA.innerHTML = "<p>"+ q.choiceA +"</p>";
-//                         choiceB.innerHTML = "<p>"+ q.choiceB +"</p>";
-//                         choiceC.innerHTML = "<p>"+ q.choiceC +"</p>";
-//                     }
+$("#submit").click (function () {
+  showResults();
+  start.style.display = "none";
+  quizContainer.style.display = "block";
+  submit.style.display = "block";
+  scoreRender.style.display = "block";
+});
+
+}
+
+let quizContainer = document.getElementById('quiz');
+
+let resultsContainer = document.getElementById('results');
+
+$("#button").click (function() {
+generateQuiz();
+start.style.display = "none";
+quizContainer.style.display = "block";
+submit.style.display = "block";
+});
+
 //   // function renderCounter() {
 //   //   if (count <= questionTime) {
 //   //     count--;
@@ -189,28 +165,7 @@ let questions = [
 //                         timer.innerHTML= ("Time left: " + count);
 //                         console.log(count);
 //                       } 
+//                       function countdown() {
+//                         setInterval(minus, 1000);
+//                       }
     
-//     function scoreRender() {
-//       scoreDiv.style.display = "block";
-//       correct.innerHTML = "Number correct= " + score;
-//       losses.innerHTML = "Number incorrect=" + (5-score);
-//     }
-  
-//     $(".answer").click(function() {
-
-//       checkAnswer();
-//     });
-    
-    
-//     function checkAnswer (answer) {
-//       if (answer == questions[runningQuestion].correct){
-//         score++;
-//       }
-//       }
-//       count = 0;
-//       if (runningQuestion < lastQuestion) {
-//         runningQuestion++;
-//         renderQuestion();
-//       }
-
-
